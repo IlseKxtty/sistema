753f42e7c83e4e535776f44a2f3ea04a377ec411
Ilse Johana Garcia Gonzalez <ilsekxtty@gmail.com>
2026-03-25 14:54:59 -0600

    feat: CRUD completo de usuarios

--- database/migrations/2026_03_25_203203_create_usuarios_table.php
+++ database/migrations/2026_03_25_203203_create_usuarios_table.php
@@ -10,14 +10,15 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre');
-            $table->string('email')->unique();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('usuarios', function (Blueprint $table) {
+        $table->id();
+        $table->string('nombre');
+        $table->string('email')->unique();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
